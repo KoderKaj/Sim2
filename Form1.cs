ffd6166cf02ea61ac907dd2f14a721eee86a58fd
Kajuran <Kajuran@DESKTOP-RMB9U75>
2024-03-07 20:03:12 +0000

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Sim2
+{
+    public partial class Form1 : Form
+    {
+        int maxX, maxY;
+        List<Test> tests = new List<Test> { };
+        List<Leader> leaders = new List<Leader>();
+        List<Brush> colours = new List<Brush>()
+        {
+            Brushes.Firebrick,
+            Brushes.Black,
+            Brushes.Red,
+            Brushes.Green,
+            Brushes.Blue,
+            Brushes.Magenta,
+            Brushes.Yellow,
+            Brushes.Navy
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            maxX = this.ClientSize.Width;
+            maxY = this.ClientSize.Height;
+            leaders.Add(new Leader(maxX / 2, maxY / 2));
+            float a = 5;
+            tests.Add(new Test(20,20).setSpeed(a));
+            a -= 0.01f;
+            tests.Add(new Test(maxX-20,20).setSpeed(a));
+            a -= 0.01f;
+            tests.Add(new Test(maxX-20,maxY-20).setSpeed(a));
+            a -= 0.01f;
+            tests.Add(new Test(20, maxY-20).setSpeed(a));
+            a -= 0.01f;
+            tests.Add(new Test(40, maxY - 40).setSpeed(a));
+            a -= 0.01f;
+            tests.Add(new Test(maxX - 40, maxY - 40).setSpeed(a));
+            a -= 0.01f;
+            tests.Add(new Test(maxX - 40, 40).setSpeed(a));
+            a -= 0.01f;
+            tests.Add(new Test(40, 40).setSpeed(a));
+            for (int i = 0; i < 8; i++)
+            {
+                tests[i].colour = colours[i];
+            }
+        }
+
+        private void update_Tick(object sender, EventArgs e)
+        {
+            Test prev = leaders[0];//tests[tests.Count-1];
+            var rand = new Random();
+            foreach (Test test in tests)
+            {
+                if (test.move(prev))
+                {
+                    test.x = rand.Next(0, maxX);
+                    test.y = rand.Next(0, maxY);
+                }
+                prev = test;
+            }
+            leaders[0].move();
+            this.Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            foreach (Test test in tests)
+            {
+                g.FillEllipse(test.colour, new RectangleF(test.x, test.y, test.size, test.size));
+            }
+            Leader temp = leaders[0];
+            g.FillEllipse(temp.colour, new RectangleF(temp.x, temp.y, temp.size, temp.size));
+        }
+    }
+    public class Cell
+    {
+        public float x, y, speed, size;
+        public Brush colour;
+        public Cell()
+        {
+
+        }
+    }
+    public class Leader : Test
+    {
+        public float midX, midY, rad = 120, speedX, speedY;
+        private bool up = false, right = true;
+        public Leader(float newX, float newY) : base(newX, newY)
+        {
+            colour = Brushes.Purple;
+            size = 30;
+            speed = 1;
+            midX = newX; 
+            midY = newY;
+            x = midX;
+            y = midY;
+        }
+        public bool move2()
+        {
+            if (x > midX + rad)
+            {
+                right = false;
+            }
+            else if (x < midX - rad)
+            {
+                right = true;
+            }
+            if (y > midY + rad)
+            {
+                up = true;
+            }
+            else if (y < midY - rad)
+            {
+                up = false;
+            }
+            float diffX, diffY;
+            if (up) { diffY = y - (midY+1-rad); }
+            else { diffY = y - (midY+1+rad); }
+            if (right) { diffX = (midX+1+rad)- x; }
+            else { diffX = (midX+1-rad) - x; }
+            float magnitude = 0.2f;
+            if (diffX + diffY > 3)
+            {
+                magnitude = (float)Math.Sqrt(diffX * diffX + diffY * diffY);
+            }
+            diffX /= magnitude; diffY /= magnitude;
+            x += diffX * speed;
+            y -= diffY * speed;
+            return false;
+        }
+        float angle = 0;
+        public void move()
+        { 
+            x = rad * (float)Math.Sin(angle) + midX;
+            y = rad * (float)Math.Cos(angle) + midY;
+            if (angle >= 360) { angle = 0; }
+            else { angle += 0.043f; } //tan-1(target speed / radius) -> (float)Math.Atan2(5,120)
+        }                   
+    }
+    public class Test : Cell
+    {
+        public Test(float newX, float newY) : base()
+        {
+            size = 30;
+            colour = Brushes.Aqua;
+            speed = 10;
+            x = newX;
+            y = newY;
+        }
+        public Test setSpeed(float newSpeed)
+        {
+            speed = newSpeed;
+            return this;
+        }
+        public bool move(Test target)
+        {
+            float diffX = (target.x + target.size / 2) - (x + size / 2);
+            float diffY = (y + size / 2) - (target.y + target.size / 2);
+            float magnitude = (float)Math.Sqrt(diffX * diffX + diffY * diffY);
+            if (magnitude > target.size / 2)
+            {
+                diffX /= magnitude;
+                diffY /= magnitude;
+                x += diffX * speed;
+                y -= diffY * speed;
+                return false;
+            }
+            else
+            {               //Collision
+                return true;
+            }
+        }
+    }
+}
